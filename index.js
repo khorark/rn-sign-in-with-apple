7acb53aab3198096c1a2e94353b5a1b71fc77c20
Arkadiy Khoroshikh <khorark@gmail.com>
2019-12-03 18:04:58 +0300

    Index. Added check try-catch when parse data.

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import {NativeModules} from 'react-native';
 const { RnSignInWithApple } = NativeModules;
 
 export const loginWithApple = async () => {
-    const data = await RnSignInWithApple.login();
-    return JSON.parse(data);
+    try {
+        const data = await RnSignInWithApple.login();
+        return JSON.parse(data);
+    } catch (e) {
+        throw new Error(e)
+    }
+
 };
